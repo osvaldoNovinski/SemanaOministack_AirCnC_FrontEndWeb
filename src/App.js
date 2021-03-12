12cdc98ff9d2d2fafc43fbf5c77b69282807aef2
Osvaldo Novinski <osvaldo.novinski@hotmail.com>
2021-03-11 23:13:43 -0300

    adicionado assets e configurações

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Hello World!!!</h1>
-      </header>
-    </div>
+   
+     <div className="container">
+       
+     </div>
+   
   );
 }
 
